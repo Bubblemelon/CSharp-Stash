430e64d13948aaaf91e64e3d183b2e95ef28d9d0
bubblemelon <c.f.2y.93@gmail.com>
2018-03-14 13:04:22 -0700

    added a reference link

--- Data Structures/howtoforeach.cs
+++ Data Structures/howtoforeach.cs
@@ -12,4 +12,5 @@
  * (2) https://bytes.com/topic/c-sharp/answers/639841-step-through-struct-using-foreach
  * (3) https://www.codeproject.com/Questions/651144/Csharp-Loop-through-all-struct-members
  * (3) https://msdn.microsoft.com/en-us/library/system.reflection.bindingflags(v=vs.110).aspx
+ * (4) https://stackoverflow.com/questions/11460103/nested-struct-as-members-in-a-class
  */
